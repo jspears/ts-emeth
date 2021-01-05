444999d79262b4967e5ac69e03b59359d49d2d3b
Justin <juspears@paypal.com>
2021-01-05 10:22:08 -0800

    upgrade

--- src/postcss-plugin.ts
+++ src/postcss-plugin.ts
@@ -1,9 +1,9 @@
-import postcss from 'postcss';
 import {generate} from './generate';
 import {EmethTSOptions} from './types';
-import {dashesCamelCase} from './util';
+import postcssModules from 'postcss-modules'
+import type * as postcss from 'postcss'
 
-const resolveMessage = (message: postcss.ResultMessage): string | undefined => {
+const resolveMessage = (message: postcss.Message): string | undefined => {
     if (message.type === 'export' && message.value) {
         return message.value.name || message.value;
     }
@@ -11,36 +11,10 @@ const resolveMessage = (message: postcss.ResultMessage): string | undefined => {
         return message.value && message.value.localName;
     }
 };
+const plugin = (opts: EmethTSOptions = {}) => ({
+    postcssPlugin: 'ts-emeth',
+    async OnceExit(root, result) {
 
-const plugin = postcss.plugin('ts-emeth', function (opts: EmethTSOptions = {}) {
-    return async (root, result) => {
-        const exports: string[] = [];
-
-        for (const message of result.messages) {
-            let name = resolveMessage(message);
-            if (name) {
-                switch (opts.localsConvention || 'camelCase') {
-                    case 'camelCase': {
-                        const cleanName = dashesCamelCase(name);
-                        if (cleanName !== name) {
-                            exports.push(name, cleanName);
-                        } else {
-                            exports.push(name);
-                        }
-                        break;
-                    }
-
-                    case 'camelCaseOnly': {
-                        exports.push(dashesCamelCase(name));
-                        break;
-                    }
-                    case 'asIs':
-                    default:
-                        exports.push(name);
-                        break;
-                }
-            }
-        }
         await generate(result.opts.to || result.opts.from, exports, opts);
     }
 });
